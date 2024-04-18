1b878b7d2329bbaa579721ce1d494ebd3095d92e
Cash Hollister <cashhollister2u@gmail.com>
2024-04-18 00:27:12 -0400

    icon and mad bio input larger

--- components/EditComponents/EditForm.tsx
+++ components/EditComponents/EditForm.tsx
@@ -329,7 +329,7 @@ void, onbackgroundPrev: (background_image:string) => void, onprofilepicPrev: (pr
             share.
           </p>
 
-          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-2">
             <div className="sm:col-span-3">
               <label
                 htmlFor="first-name"
